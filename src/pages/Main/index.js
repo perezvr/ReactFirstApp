6e4bcd975b0ddd5c5a5e31e28ff356385e845a14
Renan Perez <perez.vr@gmail.com>
2020-04-24 23:59:22 -0300

    Estilizações

---
+++ src/pages/Main/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+// import { Container } from './styles';
+
+export default function Main() {
+  return <Title error={false}>HelloWorld</Title>;
+}
