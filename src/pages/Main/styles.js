ecbf884780977ae5de008fabdb0c201319ec5c60
Renan <perez.vr@gmail.com>
2020-04-27 18:55:45 -0300

    finalização

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,31 +1,9 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 
 /**
  * Forma de definir escopo da estilização somente para o componente desejado
  * Exporta um h1 estilizado
  */
-export const Container = styled.div`
-  max-width: 700px;
-  background: #fff;
-  border-radius: 4px;
-  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-  padding: 30px;
-  margin: 80px auto;
-
-  h1 {
-    font-size: 20px;
-
-    /*Centraliza os componentes alinhados */
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-
-    svg {
-      margin-right: 10px;
-    }
-  }
-`;
-
 export const Form = styled.form`
   margin-top: 30px;
   display: flex;
@@ -41,17 +19,68 @@ export const Form = styled.form`
   }
 `;
 
-export const SubmitButton = styled.button.attrs({
+/* definindo uma estilização de rotação */
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to{
+    transform: rotate(360deg);
+  }
+`;
+
+export const SubmitButton = styled.button.attrs((props) => ({
+  /* Atributo do component button */
   type: 'submit',
-})`
+  /* Criando uma proprieddade 'disabled' a partir da propriedade 'loading' */
+  disabled: props.loading,
+}))`
   background: #7159c1;
   border: 0;
   padding: 0 15px;
   margin-left: 10px;
   border-radius: 4px;
 
-  /*Centraliza os componentes alinhados */
+  /* Centraliza os componentes alinhados */
   display: flex;
   flex-direction: center;
   align-items: center;
+
+  /* Trata a estilização do componente quando a propriedade 'disabled' = true */
+  &[disabled] {
+    cursor: not-allowed;
+    opacity: 0.6;
+  }
+
+  /* Aplicando uma estilização de rotação baseado na prop 'loading' */
+  ${(props) =>
+    props.loading &&
+    css`
+      svg {
+        animation: ${rotate} 2s linear infinite;
+      }
+    `}
+`;
+
+export const List = styled.ul`
+  list-style: none;
+  margin-top: 30px;
+
+  li {
+    padding: 15px 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    /* Aplica estilização em todos os itens menos no primeiro */
+    & + li {
+      border-top: 1px solid #eee;
+    }
+    a {
+      color: #7159c1;
+      text-decoration: none;
+    }
+  }
 `;
